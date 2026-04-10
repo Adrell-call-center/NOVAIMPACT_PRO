8943ec34a8212881cf78d5d2f6bd235c5f460524
ZakariaMegnioui <ziko17sporty@gmail.com>
2026-04-10 12:33:29 +0100

    fix: enhance language handling and content retrieval in BlogPost component
    - Refactored BlogPost component to include initial language prop for better language management.
    - Introduced getContent function to streamline content retrieval based on selected language.
    - Improved error handling for missing posts with a more user-friendly message.
    - Updated reading time calculation to ensure accurate results based on the selected content.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,36 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* LinkedIn Insight Tag */}
+      <Script id="linkedin-insight" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "8971306";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+      <Script id="linkedin-insight-loader" strategy="afterInteractive">
+        {`
+          (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=8971306&fmt=gif" />
+      </noscript>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
