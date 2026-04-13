67ba04510964d938385b30c6ef1db384a3e3c9da
ZakariaMegnioui <ziko17sporty@gmail.com>
2026-04-13 12:27:37 +0100

    perf: remove unused header/footer variants, simplify RootLayout to Header3/Footer3

--- src/components/common/layout/RootLayout.jsx
+++ src/components/common/layout/RootLayout.jsx
@@ -1,58 +1,12 @@
 import { useEffect, useRef, useState } from "react";
-import dynamic from "next/dynamic";
-import allNavData from "../../../data/navData.json";
+import Header3 from "@/components/header/Header3";
+import Footer3 from "@/components/footer/Footer3";
 import Preloader from "@/components/preloader/Preloader";
 import CommonAnimation from "../CommonAnimation";
 import ScrollSmootherComponents from "../ScrollSmootherComponents";
 import CursorAnimation from "../CursorAnimation";
 import Switcher from "../Switcher";
 import ScrollTop from "../ScrollTop";
-
-const Header1 = dynamic(() => import("@/components/header/Header1"));
-const Header2 = dynamic(() => import("@/components/header/Header2"));
-const Header3 = dynamic(() => import("@/components/header/Header3"));
-const Header4 = dynamic(() => import("@/components/header/Header4"));
-const Header5 = dynamic(() => import("@/components/header/Header5"));
-const Footer1 = dynamic(() => import("@/components/footer/Footer1"));
-const Footer2 = dynamic(() => import("@/components/footer/Footer2"));
-const Footer3 = dynamic(() => import("@/components/footer/Footer3"));
-const Footer4 = dynamic(() => import("@/components/footer/Footer4"));
-const Footer5 = dynamic(() => import("@/components/footer/Footer5"));
-
-const HeaderContent = ({ header, navData }) => {
-  if (header == "header1") {
-    return <Header1 navData={navData} />;
-  } else if (header == "header2") {
-    return <Header2 navData={navData} />;
-  } else if (header == "header3") {
-    return <Header3 />;
-  } else if (header == "header4") {
-    return <Header4 navData={navData} />;
-  } else if (header == "header5") {
-    return <Header5 />;
-  } else if (header == "none") {
-    return "";
-  } else {
-    return <Header3 />;
-  }
-};
-const FooterContent = ({ footer }) => {
-  if (footer == "footer1") {
-    return <Footer1 />;
-  } else if (footer == "footer2") {
-    return <Footer2 />;
-  } else if (footer == "footer3") {
-    return <Footer3 />;
-  } else if (footer == "footer4") {
-    return <Footer4 />;
-  } else if (footer == "footer5") {
-    return <Footer5 />;
-  } else if (footer == "none") {
-    return "";
-  } else {
-    return <Footer3 />;
-  }
-};
 
 export default function RootLayout({
   children,
@@ -66,14 +20,13 @@ export default function RootLayout({
     }
     return defaultMode;
   });
-  const [navData, setNavData] = useState({});
 
   const cursor1 = useRef();
   const cursor2 = useRef();
+
   useEffect(() => {
-    setNavData(allNavData);
     if (typeof window !== "undefined") {
-      if (mode == "dark") {
+      if (mode === "dark") {
         document.querySelector("body").classList.add("dark");
       } else {
         document.querySelector("body").classList.remove("dark");
@@ -91,6 +44,7 @@ export default function RootLayout({
       return () => window.removeEventListener("theme-change", handleThemeChange);
     }
   }, []);
+
   return (
     <>
       <CommonAnimation>
@@ -108,11 +62,11 @@ export default function RootLayout({
           cursor2={cursor2}
         />
         <ScrollTop />
-        <HeaderContent header={header} navData={navData} />
+        <Header3 />
         <div id="smooth-wrapper">
           <div id="smooth-content">
             {children}
-            <FooterContent footer={footer} />
+            {footer !== "none" && <Footer3 />}
           </div>
         </div>
       </CommonAnimation>
