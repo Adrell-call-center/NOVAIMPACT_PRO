91101acc15e3b55c98f85e03b0827936bca58eab
ZakariaMegnioui <ziko17sporty@gmail.com>
2026-04-13 11:38:25 +0100

    perf: lazy-load ScrollSmoother plugin after first paint

--- src/components/common/ScrollSmootherComponents.jsx
+++ src/components/common/ScrollSmootherComponents.jsx
@@ -1,23 +1,22 @@
 import { useEffect } from "react";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "@/plugins";
-
-gsap.registerPlugin(ScrollSmoother);
 
 const ScrollSmootherComponents = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      let device_width = window.innerWidth;
-      let tHero = gsap.context(() => {
-        ScrollSmoother.create({
-          smooth: 1,
-          effects: device_width < 1025 ? false : true,
-          smoothTouch: false,
-          normalizeScroll: false,
-          ignoreMobileResize: true,
+      import("@/plugins").then(({ ScrollSmoother }) => {
+        gsap.registerPlugin(ScrollSmoother);
+        let device_width = window.innerWidth;
+        let tHero = gsap.context(() => {
+          ScrollSmoother.create({
+            smooth: 1,
+            effects: device_width < 1025 ? false : true,
+            smoothTouch: false,
+            normalizeScroll: false,
+            ignoreMobileResize: true,
+          });
         });
       });
-      return () => tHero.revert();
     }
   }, []);
   return <div></div>;
