b4bbbc53b933425203c614c8f02e88227f2a191c
ZakariaMegnioui <ziko17sporty@gmail.com>
2026-04-13 11:49:07 +0100

    perf: replace Google Fonts link tag with next/font for zero layout shift

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
+import { Kanit } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../../public/assets/scss/master.scss"
+import "../../public/assets/scss/master.scss";
 import "@/styles/extra.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-kanit",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={kanit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
